7d7d7f3e57e48aa01f91c5179b5fdfd1ce8badba
Mohamed Magdy <midomohamedmagdy@hotmail.com>
2025-08-17 08:05:09 +0300

    Minor changes

--- src/Routing/Paths.jsx
+++ src/Routing/Paths.jsx
@@ -3,12 +3,12 @@ import UserContextProvider from '../context/userContext.jsx'
 import Home from '../Pages/Home/Home'
 import AppLayout from '../Pages/Layout/AppLayout'
 import LogIn from '../Pages/LogIn/LogIn.jsx'
-import MyProfile from '../Pages/MyProfile/MyProfile.jsx'
 import NotFound from '../Pages/NotFound/NotFound.jsx'
 import Register from '../Pages/Register/Register.jsx'
 import Auth from './Auth.jsx'
 import Protected from './Protected.jsx'
 import SinglePost from '../Pages/Posts/SinglePost.jsx'
+import ProfilePage from '../Pages/Profile/ProfilePage.jsx'
 
 
 export const layout = createBrowserRouter([
@@ -19,7 +19,7 @@ export const layout = createBrowserRouter([
             </UserContextProvider>
         , children: [
             { index: true, element: <Protected> <Home /> </Protected> },
-            { path: '/profile/:name', element: <Protected> <MyProfile /> </Protected> },
+            { path: '/profile/:name', element: <Protected> <ProfilePage /> </Protected> },
             { path: '/posts/:id', element: <Protected> <SinglePost /> </Protected> },
             { path: '/login', element: <Auth><LogIn /></Auth> },
             { path: '/register', element: <Auth><Register /></Auth> },
